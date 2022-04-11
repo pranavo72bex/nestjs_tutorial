0288c45e60691f67cc1c421f41e446ecdde28683
pranavo72bex <pranav072bex@ioepc.edu.np>
2022-04-11 10:50:18 +0545

    chore:validation

--- src/crud/app.controller.ts
+++ src/crud/app.controller.ts
@@ -1,31 +1,28 @@
-import { Controller, Delete, Get, Param, Post, Put,Body, HttpCode} from "@nestjs/common";
-import { type } from "os";
+import { Controller, Delete, Get, Param, Post, Put,Body, HttpCode,ParseUUIDPipe,ParseEnumPipe} from "@nestjs/common";
 import{data, ReportType} from "src/testdata/data"
-
 import { AppService } from "./app.service";
 @Controller('/report/:type')
 export class AppController{
   constructor(private readonly appService:AppService){}
   @Get()
-  getAllReport(@Param('type') type:string){
+  getAllReport(@Param('type',new ParseEnumPipe(ReportType)) type:string){
     const reportType = type === "income" ? ReportType.INCOME : ReportType.EXPENSE;
     return this.appService.getAllReport(reportType)
   }
   @Get(':id')
-  getById(@Param('type')type :string,@Param('id')id:string){
+  getById(@Param('type')type :string,@Param('id',ParseUUIDPipe)id:string){
     const reportType = type === "income" ? ReportType.INCOME : ReportType.EXPENSE;
     this.appService.getById(reportType,id)
   }
   @Post()
   createReport(@Body(){source,amount}: {amount:number;source:string},@Param('type')type:string){
     const reportType = type === "income" ? ReportType.INCOME : ReportType.EXPENSE;
-   this.appService.createReport(reportType,{source,amount})
-    
+   this.appService.createReport(reportType,{source,amount}) 
   }
   @HttpCode(204)
   @Delete(':id')
   deleteReport(
-    @Param('id') id:string,
+    @Param('id',ParseUUIDPipe) id:string,
   ){
    this.appService.deleteReport(id)
   }
