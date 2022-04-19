f84eb6266178bc2703aeb9350e63f3b704a45794
pranavo72bex <pranav072bex@ioepc.edu.np>
2022-04-19 18:17:33 +0545

    feat:implementation of strategy

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,4 +1,7 @@
 import { Module } from '@nestjs/common';
+import { UserController } from './user.controller';
 
-@Module({})
-export class UserModule {}
+@Module({
+    controllers: [UserController]
+})
+export class UserModule { }
