ef238d7294483f0cd23342aa8a3d2d38a02c8b43
pranavo72bex <pranav072bex@ioepc.edu.np>
2022-04-22 09:52:22 +0545

    chore:custom param

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,13 +1,13 @@
 import { Controller, Get, Req, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
-import { Request } from 'express';
+import { User } from '@prisma/client';
+import { GetUser } from 'src/auth/decorator/get-user.decorator';
 
-
+@UseGuards(AuthGuard('jwt'))
 @Controller('users')
 export class UserController {
-    @UseGuards(AuthGuard('jwt'))
     @Get('me')
-    getMe(@Req() req: Request) {
-        return req.user;
+    getMe(@GetUser() user: User) {
+        return user;
     }
 }
