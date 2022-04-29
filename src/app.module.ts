052cd7c1257548709585c7545ef23b11db7db8f7
pranavo72bex <pranav072bex@ioepc.edu.np>
2022-04-29 08:53:07 +0545

    chore:refactor

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,7 @@ import { BookmarkModule } from './bookmark/bookmark.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { ConfigModule } from '@nestjs/config';
 import { CrudModule } from './crud/crud.module';
-import { MessagesModule } from './messages/messages.module';
+import { MessagesModule } from './chat_app_websocket/messages.module';
 import { GoogleOauthModule } from './google-oauth/oauth.module';
 import { facebookAuthModule } from './facebook-oauth/facebook.module';
 
